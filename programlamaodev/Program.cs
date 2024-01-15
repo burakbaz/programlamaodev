5442a19613b85b02e7c2de2a2b84865c76500b8b
CHİEH-SHE-SHUAİ <CHEH-SHE-SHUA@DESKTOP-RDAQ55L>
2024-01-15 12:41:44 +0300

    Proje dosyası ekle.

---
+++ programlamaodev/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Başlangıç değeri: ");
+        int baslangic = int.Parse(Console.ReadLine());
+
+        Console.Write("Bitiş değeri: ");
+        int bitis = int.Parse(Console.ReadLine());
+
+        Console.Write("Oluşturulacak sayı adedi: ");
+        int sayiAdedi = int.Parse(Console.ReadLine());
+
+        Random rnd = new Random();
+        HashSet<int> essizsayilar = new HashSet<int>();
+
+        while (essizsayilar.Count < sayiAdedi)
+        {
+            int rasgeleSayi = rnd.Next(baslangic, bitis + 1);
+            essizsayilar.Add(rasgeleSayi);
+        }
+
+        int[] rasgeleSayilar = essizsayilar.ToArray();
+
+
+        for (int i = 0; i < rasgeleSayilar.Length; i++)
+        {
+            Console.WriteLine(rasgeleSayilar[i]);
+        }
+
+        
+
+    }
+}
